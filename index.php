f8df2b793656adc55c28cff0e08c0be751d20c14
alexandrejoao <xandybrown.aj@gmail.com>
2022-06-14 00:02:32 -0300

    teste

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+require 'index.php';
 require 'inc/configuration.php';
 require 'inc/Slim-2.x/Slim/Slim.php';
 
@@ -12,7 +13,7 @@ $app->get(
     '/',
     function () {
 
-        require_once("index.php");
+        require_once("view/index.php");
     }
 );
 
