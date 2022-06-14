fe31abce19c18e8efd55f27954dad308c47e9877
alexandrejoao <xandybrown.aj@gmail.com>
2022-06-13 23:52:52 -0300

    Atualizado mysql

--- inc/configuration.php
+++ inc/configuration.php
@@ -2,6 +2,8 @@
 
 session_start();
 
+require_once 'inc/configuration.php';
+
 
 class Sql
 {
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once 'inc/configuration.php';
 require 'inc/configuration.php';
 require 'inc/Slim-2.x/Slim/Slim.php';
 
