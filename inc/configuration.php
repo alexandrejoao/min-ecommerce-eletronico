1d3c157ed9b84695be6c35ab748eabf4fb2a2a55
alexandrejoao <xandybrown.aj@gmail.com>
2022-06-13 23:34:10 -0300

    atualizando conecção Mysql

--- inc/configuration.php
+++ inc/configuration.php
@@ -2,14 +2,26 @@
 
 session_start();
 
+
 class Sql
 {
+    //host
+    private $host = 'us-cdbr-east-05.cleardb.net';
+
+    //usuario
+    private $usuario = 'b73247dfff2465';
+
+    //senha
+    private $senha = 'f0633a1c';
+
+    //banco de dados
+    private $database = 'heroku_cc35a753502f7d0';
 
     public $conn;
 
     public function __construct()
     {
-        return $this->conn = mysqli_connect();
+        return $this->conn = mysqli_connect($this->host, $this->usuario, $this->senha, $this->database);
     }
 
     public function query($string_query)
